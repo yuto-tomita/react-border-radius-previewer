4c5e05ac03f5f9c2785be7986e51ca32416173a7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-09-12 20:19:31 +0900

    Rangeのデザイン加筆

--- src/components/ui/InputRange/InputRange.tsx
+++ src/components/ui/InputRange/InputRange.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from 'react'
+import style from './InputRange.module.css'
 
 interface Props {
 	onChange: (arg: number) => void
-	value: number
+  value: number
+  className?: string
 }
 
 const InputRange: FC<Props> = ({
   onChange,
-  value
+  value,
+  className = ''
 }) => {
+  const parentClassName = className || ''
   return (
     <input
       type="range"
       onChange={(e) => onChange(Number(e.currentTarget.value))}
       value={value}
-      className="mb-6 w-full h-0.5 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+      // className="mb-6 w-full h-0.5 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+      className={`${style} ${parentClassName}`}
       min="0"
       max="50"
     />
